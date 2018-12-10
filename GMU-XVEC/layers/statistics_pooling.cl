ed51292e06481c87d0de0e5e565892ea63bf9419
Jan Profant <jan.profant@phonexia.com>
2018-12-10 22:30:20 +0100

    wip

--- GMU-XVEC/layers/statistics_pooling.cl
+++ GMU-XVEC/layers/statistics_pooling.cl
@@ -1,12 +1,11 @@
-__kernel void pool_statistics(float variance_floor, unsigned long dim, unsigned long rows, __global float *tosum, __global float *val, __global float *output) {
+__kernel void pool_statistics(float variance_floor, unsigned long dim, unsigned long rows, __global float *val, __global float *output) {
     const int globalId = get_global_id(0);
     unsigned long cols = dim * 2 + 1;
     if (globalId >= cols) {
         return;
     }
     
-    val[globalId] = tosum[cols * rows + globalId] - tosum[globalId];
-    float cnt = tosum[cols * rows] - tosum[0];
+    float cnt = val[0];
 
     if (globalId >= dim) {
         float mean_val = val[globalId - dim] / cnt;
--- GMU-XVEC/layers/statistics_pooling.cpp
+++ GMU-XVEC/layers/statistics_pooling.cpp
@@ -14,13 +14,10 @@
 
 
 std::vector<float> StatisticsPoolingLayer::forward(std::vector<float> input, unsigned long &rows, unsigned long &cols, cl_device_id device, cl_context context) {
-    for (unsigned long i = 0; i < cols; i++)
-        input.insert(input.begin(), 0.0f);
-    auto start = left_context == 0 ? input.begin() : input.begin() + left_context * cols;
-    auto end = right_context > rows ? input.end() : input.begin() + right_context * cols;
-    std::vector<float> tosum(end - start);
-    std::copy(start, end, tosum.begin());
+    assert(left_context == 0);
+    assert(right_context > rows);
     std::vector<float> val(cols);
+    std::copy(input.end() - cols, input.end(), val.begin());
     unsigned long dim = (cols - 1) / 2;
     std::vector<float> output(dim * 2);
     
@@ -29,12 +26,7 @@ std::vector<float> StatisticsPoolingLayer::forward(std::vector<float> input, uns
     cl_program program;
     cl_kernel extraction_kernel = compile_kernel(device, context, "layers/statistics_pooling.cl", "pool_statistics", max_local_size, program);
     
-    // initialize input an output buffers
-    cl_mem tosum_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * tosum.size(), &tosum[0], &err);
-    if (err < 0) {
-        std::cerr << getCLError(err) << std::endl;
-        exit(1);
-    }
+    // initialize input and output buffers
     cl_mem val_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(float) * val.size(), &val[0], &err);
     if (err < 0) {
         std::cerr << getCLError(err) << std::endl;
@@ -57,9 +49,8 @@ std::vector<float> StatisticsPoolingLayer::forward(std::vector<float> input, uns
     err |= clSetKernelArg(extraction_kernel, 0, sizeof(float), &variance_floor);
     err |= clSetKernelArg(extraction_kernel, 1, sizeof(unsigned long), &dim);
     err |= clSetKernelArg(extraction_kernel, 2, sizeof(unsigned long), &rows);
-    err |= clSetKernelArg(extraction_kernel, 3, sizeof(cl_mem), &tosum_buffer);
-    err |= clSetKernelArg(extraction_kernel, 4, sizeof(cl_mem), &val_buffer);
-    err |= clSetKernelArg(extraction_kernel, 5, sizeof(cl_mem), &output_buffer);
+    err |= clSetKernelArg(extraction_kernel, 3, sizeof(cl_mem), &val_buffer);
+    err |= clSetKernelArg(extraction_kernel, 4, sizeof(cl_mem), &output_buffer);
     if (err < 0) {
         std::cerr << getCLError(err) << std::endl;
         exit(1);
@@ -83,7 +74,6 @@ std::vector<float> StatisticsPoolingLayer::forward(std::vector<float> input, uns
     }
     
     clReleaseMemObject(val_buffer);
-    clReleaseMemObject(tosum_buffer);
     clReleaseMemObject(output_buffer);
     clReleaseCommandQueue(queue);
     clReleaseKernel(extraction_kernel);
